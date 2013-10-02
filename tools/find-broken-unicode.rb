1b642a494b4948a4d1c1defe3595f6bbf930579c
Charles Hooper <chooper@plumata.com>
2013-10-01 22:38:56 -0700

    add tool to check if all of the files contain valid unicode

---
+++ tools/find-broken-unicode.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+# Crawls given dir looking for *.(md|markdown) files that contain invalid unicode
+
+raise "Usage: #{$0} <dir>" if ARGV[0].nil?
+
+target_globs = [
+  File.join(ARGV[0], "**", "*.yml"),
+  File.join(ARGV[0], "**", "*.htm"),
+  File.join(ARGV[0], "**", "*.html"),
+  File.join(ARGV[0], "**", "*.md"),
+  File.join(ARGV[0], "**", "*.markdown") ]
+
+target_globs.each do |target_glob|
+  Dir.glob(target_glob).each do |f|
+    puts "not a file! #{f}" unless File.file?(f)
+
+    contents = open(f).read
+    puts "#{contents.valid_encoding?}\t#{f}"
+  end
+end
+
